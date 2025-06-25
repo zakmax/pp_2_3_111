6a0969977b3f0b43cfa8b4c4fe6c30876ebba8df
zak-max15@yandex.ru <java180490>
2025-06-25 18:34:40 +0300

    CRUD работает на персон

--- src/main/java/ru/dao/PersonDAO.java
+++ src/main/java/ru/dao/PersonDAO.java
@@ -1,43 +1,52 @@
 package ru.dao;
 
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import ru.models.Person;
-//
-//import java.util.ArrayList;
-//import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.models.Person;
 
-//@Component
-//public class PersonDAO {
-//    private static int PEOPLE_COUNT;
-//    private List<Person> people;
-//
-//    {
-//        people = new ArrayList<>();
-//
-//        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-//        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-//        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-//        people.add(new Person(++PEOPLE_COUNT, "Katy"));
-//    }
-//
-//    public List<Person> index() {
-//
-//        return people;
-//    }
-//
-//    public Person show(int id) {
-//
-//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-//
-//    @GetMapping("/new")
-//    public String newPerson(Model model ){
-//        model.add
-//        }
-//
-//        @GetMapping()
-//        public void save( Person person) {
-//        people.add(person);
-//        }
-//    }
-//}
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+
+        return people;
+    }
+
+    public Person show(int id) {
+
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+
+
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatePerson.getName());
+
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
+
+}
